080b199b9de038aa5c4558473d41bd51683bd2ac
Benjamin Krier <benkrier@gmail.com>
2018-06-27 16:25:53 -0500

    lift state to Table component

--- src/App.js
+++ src/App.js
@@ -34,9 +34,7 @@ class App extends Component {
       searchKey: "",
       searchTerm: DEFAULT_QUERY,
       error: null,
-      isLoading: false,
-      sortKey: "NONE",
-      isSortReverse: false
+      isLoading: false
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -45,7 +43,6 @@ class App extends Component {
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
-    this.onSort = this.onSort.bind(this);
   }
 
   needsToSearchTopStories(searchTerm) {
@@ -99,12 +96,6 @@ class App extends Component {
     this.setState({
       results: { ...results, [searchKey]: { hits: updatedHits, page } }
     });
-  }
-
-  onSort(sortKey) {
-    const isSortReverse =
-      this.state.sortKey === sortKey && !this.state.isSortReverse;
-    this.setState({ sortKey, isSortReverse });
   }
 
   componentDidMount() {
